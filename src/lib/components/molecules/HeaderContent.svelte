69b911f0b1dcb5ba374d2695f0abf3a890b50219
Sanne van Seeventer <sanne.van.seeventer@hva.nl>
2024-05-16 18:31:59 +0200

    Add Icon component

--- src/lib/components/molecules/HeaderContent.svelte
+++ src/lib/components/molecules/HeaderContent.svelte
@@ -1,4 +1,6 @@
 <script>
+  import IconLocation from '../atoms/IconLocation.svelte'
+
   const mockData = [
     {
       title: 'Regular',
@@ -33,11 +35,7 @@
   <h1 class="subtitle">{mockData[0].subtitle}</h1>
 
   <div class="location-price">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15"
-      ><path
-        d="M12 .042a9.992 9.992 0 0 0-9.981 9.98c0 2.57 1.99 6.592 5.915 11.954a5.034 5.034 0 0 0 8.132 0c3.925-5.362 5.915-9.384 5.915-11.954A9.992 9.992 0 0 0 12 .042ZM12 14a4 4 0 1 1 4-4 4 4 0 0 1-4 4Z"
-      /></svg
-    >
+    <IconLocation width="15" height="15" color="hsl(21.23, 100%, 87.25%)" />
     <p class="location">{mockData[0].location}</p>
     <p class="price">{mockData[0].price}</p>
   </div>
@@ -78,14 +76,10 @@
     align-items: center;
   }
 
-  svg {
-    fill: var(--accent2-tertiary);
-    margin-right: 0.5rem;
-  }
-
   .location {
     color: var(--accent2-tertiary);
     margin-right: 1rem;
+    margin-left: 0.5rem;
   }
 
   .price {
