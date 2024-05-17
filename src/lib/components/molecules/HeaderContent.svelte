0f1f21d5cdf6bb215c5711f60a7963ec0b8f0393
Sanne van Seeventer <sanne.van.seeventer@hva.nl>
2024-05-17 11:15:53 +0200

    Add comments and property order

--- src/lib/components/molecules/HeaderContent.svelte
+++ src/lib/components/molecules/HeaderContent.svelte
@@ -1,6 +1,7 @@
 <script>
   import IconLocation from '../atoms/IconLocation.svelte'
 
+  // Mockdata voor de header text
   const mockData = [
     {
       title: 'Regular',
@@ -26,20 +27,23 @@
   ]
 </script>
 
-<div class="content-container">
+<section class="content-container">
+  <!-- First content row -->
   <div class="title-label">
     <h1>{mockData[0].title}</h1>
     <span class="label" aria-hidden="true">{mockData[0].label}</span>
   </div>
 
+  <!-- Second content row -->
   <h1 class="subtitle">{mockData[0].subtitle}</h1>
 
+  <!-- Last content row -->
   <div class="location-price">
     <IconLocation width="15" height="15" color="hsl(21.23, 100%, 87.25%)" />
     <p class="location">{mockData[0].location}</p>
     <p class="price">{mockData[0].price}</p>
   </div>
-</div>
+</section>
 
 <style>
   .content-container {
@@ -49,6 +53,15 @@
     margin: 5rem 2rem;
   }
 
+  /* First content row */
+  h1 {
+    display: flex;
+    align-items: center;
+    font-size: 2rem;
+    text-transform: uppercase;
+    color: white;
+  }
+
   .title-label {
     display: flex;
     align-items: center;
@@ -56,45 +69,39 @@
   }
 
   .label {
-    color: var(--accent2-tertiary);
-    font-size: 1rem;
-    text-align: center;
     padding: 0.3rem 0.5rem;
     border: 1px solid var(--accent2-tertiary);
-    font-weight: 600;
     border-radius: 3rem;
+    font-size: 1rem;
+    font-weight: 600;
+    text-align: center;
+    color: var(--accent2-tertiary);
   }
 
+  /* Second content row */
   .subtitle {
-    text-transform: uppercase;
     font-size: 2rem;
+    text-transform: uppercase;
     color: white;
   }
 
+  /* Last content row */
   .location-price {
     display: flex;
     align-items: center;
   }
 
   .location {
+    margin: 0 1rem 0 0.5rem;
     color: var(--accent2-tertiary);
-    margin-right: 1rem;
-    margin-left: 0.5rem;
   }
 
   .price {
-    color: var(--accent2-primary);
     font-weight: 600;
+    color: var(--accent2-primary);
   }
 
-  h1 {
-    display: flex;
-    align-items: center;
-    text-transform: uppercase;
-    font-size: 2rem;
-    color: white;
-  }
-
+  /* Large screens */
   @media screen and (min-width: 768px) {
     .content-container {
       margin: 8rem 8rem;
@@ -104,6 +111,10 @@
       font-size: 3rem;
     }
 
+    .label {
+      padding: 0.5rem 0.8rem;
+      font-size: 1.3rem;
+    }
     .subtitle {
       font-size: 3rem;
     }
@@ -114,11 +125,6 @@
 
     .price {
       font-size: 1.3rem;
-    }
-
-    .label {
-      font-size: 1.3rem;
-      padding: 0.5rem 0.8rem;
     }
   }
 </style>
