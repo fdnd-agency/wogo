9e6a75d5d525827cf19427842ca5b217a17cfe9b
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2024-06-02 14:52:40 +0200

    Fix: styling styles for header navigation

--- src/lib/components/atoms/icons/CartIcon.svelte
+++ src/lib/components/atoms/icons/CartIcon.svelte
@@ -1,37 +1,41 @@
 <script>
+  import { BaseButton } from '$lib/index'
+  export let width = ''
+  export let height = ''
+  export let fill = ''
 </script>
 
-<button onclick="addToCart()" aria-label="Voeg toe aan winkelwagen">
-  <svg width="50" height="50" viewBox="0 0 26 30" fill="none" xmlns="http://www.w3.org/2000/svg"
-    ><g filter="url(#filter0_d_1593_3808)"
-      ><path
+<div class="button-cart">
+  <BaseButton type="button">
+    <svg {width} {height} viewBox="0 0 26 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
         d="M21.182 5.5h-3.477v-.44C17.704 2.266 15.598 0 13 0S8.295 2.266 8.295 5.06v.44H4.818c-.452 0-.818.393-.818.88v14.74c0 .487.366.88.818.88h16.364c.452 0 .818-.393.818-.88V6.38c0-.487-.366-.88-.818-.88Zm-5.318 0h-5.728v-.44c0-1.702 1.281-3.08 2.864-3.08s2.864 1.378 2.864 3.08v.44Z"
-        fill="#FFA378"
-      /></g
-    ></svg
-  >
-  <span class="cart-counter" id="cart-counter">0</span>
-</button>
+        {fill}
+      />
+    </svg>
+  </BaseButton>
+  <span class="button-cart-counter" id="cart-counter">0</span>
+</div>
 
 <style>
-  button {
+  .button-cart {
     position: relative;
-    display: inline-flex;
+    display: flex;
     align-items: center;
-    background: none;
-    border: none;
-    cursor: pointer;
-    font-size: 24px;
+    justify-content: center;
   }
 
-  span {
+  .button-cart-counter {
     position: absolute;
-    top: 17px;
-    right: -10px;
-    background: #fff;
-    color: var(--accent2-primary);
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: white;
     border-radius: 50%;
-    padding: 0.2em 0.5em;
-    font-size: 0.7em;
+    padding: 0.5em;
+    font-size: 0.75rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
