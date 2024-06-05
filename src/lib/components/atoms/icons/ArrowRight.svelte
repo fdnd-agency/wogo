256a0f07f9cc7365f71c9ab9b9b4ed09a1a0b395
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2024-06-05 22:47:42 +0200

    Build: add prop for stroke color and set a default color

--- src/lib/components/atoms/icons/ArrowRight.svelte
+++ src/lib/components/atoms/icons/ArrowRight.svelte
@@ -1,13 +1,13 @@
 <script>
   export let width = ''
   export let height = ''
-  export let color = ''
+  export let color = 'var(--btn-primary-text-clr)'
 </script>
 
-<svg {width} {height} fill={color} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+<svg {width} {height} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
   <path
     d="M6 12h12m0 0-5-5m5 5-5 5"
-    stroke="#000"
+    stroke={color}
     stroke-width="2"
     stroke-linecap="round"
     stroke-linejoin="round"
