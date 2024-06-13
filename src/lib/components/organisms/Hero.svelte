c05c2c00167aa777e282cf279880f76d8a86f126
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2024-06-13 11:18:31 +0200

    Add subtitle data to hero section

--- src/lib/components/organisms/Hero.svelte
+++ src/lib/components/organisms/Hero.svelte
@@ -24,7 +24,7 @@
       {items[0].title}
       <PrimaryButton title="Book a Walk" icon={ArrowRight} size="lg" type="button" on:click />
     </h1>
-    <p></p>
+    <p>{items[0].subtitle}</p>
   </div>
 </section>
 
@@ -82,7 +82,7 @@
     font-size: 1.5rem;
     font-weight: 700;
     letter-spacing: 0.2rem;
-    padding: 0.5rem;
+    padding: 1.5rem 0;
     filter: drop-shadow(0 0 0.4rem #000);
     position: relative;
     animation-name: content;
