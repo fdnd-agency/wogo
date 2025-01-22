5ec2e3a9fc531debf07b3787c2e164d451d491a0
Latoya <76912011+latoyaln@users.noreply.github.com>
2025-01-22 17:50:36 +0100

    #49

--- src/lib/components/pages/FrequentlyAsked.svelte
+++ src/lib/components/pages/FrequentlyAsked.svelte
@@ -1,87 +1,106 @@
 <script>
-    import {HeadLine} from '$lib/index'
-    export let items;
-  </script>
-  
-  <svelte:head>
-    <title>Veelgestelde Vragen</title>
-  </svelte:head>
-  
-  <section id="FAQ" class="faq-section">
-    
-    <article class="faq-vragen">
-        <HeadLine title="Veelgestelde Vragen" />
+  import { HeadLine } from '$lib/index'
+  export let items;
+</script>
+
+<svelte:head>
+  <title>Veelgestelde Vragen</title>
+</svelte:head>
+
+<section id="FAQ" class="faq-section">
+  <article class="faq-vragen">
+      <h1>Veelgestelde Vragen</h1>
         {#each items[0].componentsCollection.items as item}
           <div class="faq-item">
-            <details>
-              <summary>
-                <p class="question-title">{item.title}</p>
-              </summary>
-              <p>{item.textParagraph}</p>
-            </details>
+              <details>
+                  <summary>
+                      <p class="question-title">{item.title}</p>
+                  </summary>
+                  <p>{item.textParagraph}</p>
+              </details>
           </div>
-        {/each}
-    </article>
-   
-  </section>
-  
-  <style>
-    .faq-section {
-      background-color: #3F2B21;
+      {/each}
+  </article>
+</section>
+
+<style>
+
+.faq-section {
       color: white;
       height: 100%;
-      padding: 2rem;
-    }
+      padding: 1em;
+      max-width: 90em;
+      margin: 0 auto; 
 
-    .faq-vragen{
-      width: 80%;
-      padding: 2rem;
-      background-color: #000;
+      @media (max-width: 1280px) {
+        padding: 2em; 
+      }
+  }
+
+  .faq-vragen {
+      padding: 2em;
+      background-color: #3F2B21;
       border-radius: 8px;
-    }
-  
-    .faq-item {
-      margin-bottom: 1rem;
-    }
-  
-    details {
-      background: none;
-      border: none;
-      padding: 0;
-      cursor: pointer;
-    }
-  
-    summary {
-        list-style: none;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 10px 15px;
-        font-weight: bold;
-    }
+      box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.25);
+      max-width: 100%; 
 
-    summary::after {
+      & h1{
+        font-size: var(--fs-lg);
+        color: white;
+        text-align: center;
+        margin-bottom: 1em;
+        
+        @media (max-width: 1280px) {
+          font-size: var(--fs-xl);
+          margin-bottom: 1.5em;
+        }
+      }
+      
+      @media (max-width: 1280px) {
+        padding: 2em;
+      }
+  }
+
+  .faq-item {
+      margin-bottom: 1rem;
+      border-bottom: 1px solid var(--accent2-primary); 
+      padding-bottom: 1rem;
+    
+      &  summary {
+      list-style: none;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding-top: 1rem;
+      font-weight: bold;
+      color: white;
+      flex-wrap: nowrap;
+      justify-content: flex-start; 
+
+      &::after{
         content: '';
         width: 20px;
         height: 20px;
         background: url('../../assets/plus.svg') no-repeat;
-        background-size: cover;
+        background-size: contain;
         transition: 0.2s;
-    }
+        flex-shrink: 0;
+        margin-left: 10px; 
+      }
 
-    details[open] > summary::after {
-        background: url('../../assets/min.svg') no-repeat;
-        background-size: cover;
-        transition: 0.5s;
-    }
+      &::-webkit-details-marker {
+      display: none;
+      }
 
-    summary::-webkit-details-marker {
-        display: none;
-    }
-
-    .question-title {
+      &  .question-title {
       flex-grow: 1;
+      }
     }
-  
-  </style>
-  
+  }
+
+  details[open] > summary::after {
+      background: url('../../assets/min.svg') no-repeat;
+      background-size: contain;
+      transition: 0.5s;
+  }
+</style>
