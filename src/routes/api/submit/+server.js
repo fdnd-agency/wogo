31eb6a0aa79f0b2c94367d7437b35cf0a26028e3
yassir bghiel <yassir.bghiel@hva.nl>
2025-06-07 15:27:06 +0200

    fix

--- src/routes/api/submit/+server.js
+++ src/routes/api/submit/+server.js
@@ -1,4 +1,4 @@
-import { json } from '@sveltejs/kit';
+  import { json } from '@sveltejs/kit';
 import { parse } from 'querystring';
 import { CONTENTFUL_SPACE_ID, CONTENTFUL_MANAGEMENT_TOKEN } from '$env/static/private';
 
